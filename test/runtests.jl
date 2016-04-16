0da45bc73ca72a442ed94f6e55eff36bf4d4a4ba
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-16 17:10:40 +0900

    tests from opencv.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
+using CVImgCore
 using CVImgProc
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@testset "UMat{T,N}" begin
+    umat = UMat{UInt8}(3, 3)
+    mat = Mat(umat)
+    @test isa(mat, Mat)
+    @test size(umat) == (3, 3)
+
+    # make sure resize doesn't change the type of mat
+    umat_resized = resize(umat, size(umat))
+    @test isa(umat_resized, UMat)
+    @test size(umat) == size(umat_resized)
+end
